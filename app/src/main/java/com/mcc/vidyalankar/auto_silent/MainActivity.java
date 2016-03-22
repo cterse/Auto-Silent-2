4e4b17dfa4e06ce8b71b2358e134871a0e689b7b
neil4droid <cterse@gmail.com>
2016-03-22 20:24:59 +0530

    Location Permissions

--- app/src/main/java/com/mcc/vidyalankar/auto_silent/MainActivity.java
+++ app/src/main/java/com/mcc/vidyalankar/auto_silent/MainActivity.java
@@ -7,26 +7,56 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
+
+
+public class MainActivity extends AppCompatActivity implements GoogleApiClient.OnConnectionFailedListener, GoogleApiClient.ConnectionCallbacks {
 
     private static TextView logs_textview;
     private static ImageView logs_arrow;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addOnConnectionFailedListener(this)
+                    .addConnectionCallbacks(this)
+                    .addApi(LocationServices.API)
+                    .build();
+        }
     }
 
+    //Function to open Settings
     public void openSettings(View view){
         Intent settings_intent = new Intent(this, SettingsActivity.class);
         startActivity(settings_intent);
     }
 
+    //Function to open Logs
     public void openLogs(View view){
         Intent logs_intent = new Intent(this,LogsActivity.class);
         startActivity(logs_intent);
     }
 
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
 }
